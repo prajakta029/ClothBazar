71d72209ba7b5bd9258db06d8bf42b205529b84c
yashashwi sinha <yashashwi6@gmail.com>
2021-03-04 13:08:16 +0000

    Social media(Facebook) Configuration in Database

---
+++ Rough/Queries/Configuration-queries.sql
@@ -0,0 +1,7 @@
+﻿select * from Configs
+
+insert into Configs ([Key], [value]) values ('FacebookURL', 'https://www.facebook.com/yashashwi4/')
+
+insert into Configs ([Key], [value]) values ('CompanyAddress', 'CLOTH STORE')
+
+insert into Configs ([Key], [value]) values ('CompanyPhone', '91 987 987652')

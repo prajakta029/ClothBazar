d94837396d8b3db52afaf37b482f0cf4a5b88bb3
ConfusedKaran18 <karan.s.jawale@gmail.com>
2021-04-01 01:27:42 +0530

    OrdersDone

---
+++ ClothBazar.Web/ViewModels/OrderViewModels.cs
@@ -0,0 +1,23 @@
+﻿using ClothBazar.Entities;
+using ClothBazar.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClothBazar.Web.ViewModels
+{
+    public class OrderViewModels
+    {
+        public List<Order> Orders { get; set; }
+        public string Status { get; set; }
+        public string UserID { get; set; }
+    }
+
+    public class OrderdetailsViewModels
+    {
+        public List<string> AvailableStatuses { get; set; }
+        public Order Order { get; set; }
+        public ApplicationUser OrderedBy { get; set; }
+    }
+}
